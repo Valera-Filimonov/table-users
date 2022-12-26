4d432bf7058605985a72e70ded49af4e352131a7
Valera-Filimonov <68681899+Valera-Filimonov@users.noreply.github.com>
2022-12-26 20:33:35 +0300

    feat: added handlerOnChange in input and checkBox

--- src/component/Modal/index.tsx
+++ src/component/Modal/index.tsx
@@ -1,9 +1,12 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
+import {Checkbox} from '@mui/material';
 import styles from './modal.module.css'
-import ButtonAdd from "./ButtonAdd";
-import ButtonClose from "./ButtonClose";
+import ButtonAdd from './ButtonAdd';
+import ButtonClose from './ButtonClose';
+import {ChangeEvent, useState} from "react";
 
 type PropsModalAddUser = {
     open: boolean;
@@ -25,6 +28,38 @@ const style = {
 
 const ModalAddUser = ({open, setOpen}: PropsModalAddUser): JSX.Element => {
 
+    const [id, setId] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [name, setName] = useState<string>('');
+    const [lastName, setLastName] = useState<string>('');
+    const [birthDate, setBirthDate] = useState<string>('')
+    const [access, setAccess] = useState<boolean>(false);
+
+    const handlerOnChangeId = (event: ChangeEvent<HTMLInputElement>) => {
+        setId(event.target.value);
+        console.log(id)
+    }
+    const handlerOnChangeEmail = (event: ChangeEvent<HTMLInputElement>) => {
+        setEmail(event.target.value);
+        console.log(email)
+    }
+    const handlerOnChangeName = (event: ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+        console.log(name)
+    }
+    const handlerOnChangeLastName = (event: ChangeEvent<HTMLInputElement>) => {
+        setLastName(event.target.value);
+        console.log(lastName)
+    }
+    const handlerOnChangeBirthDate = (event: ChangeEvent<HTMLInputElement>) => {
+        setBirthDate(event.target.value);
+        console.log(birthDate)
+    }
+    const handlerOnChangeAccess = (event: ChangeEvent<HTMLInputElement>) => {
+        setAccess(event.target.checked)
+        console.log(access)
+    }
+
     const handlerOnClose = () => {
         setOpen(!open)
     }
@@ -38,12 +73,67 @@ const ModalAddUser = ({open, setOpen}: PropsModalAddUser): JSX.Element => {
                 aria-describedby="modal-modal-description"
             >
                 <Box sx={style}>
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                    <Typography id="modal-modal-title"
+                                className={styles.title}
+                                variant="h6"
+                                component="h2"
+                    >
                         Добавить пользователя
                     </Typography>
-                    <Typography id="modal-modal-description" sx={{mt: 2}}>
-                        Какой-то текст
-                    </Typography>
+                    <div className={styles.input}>
+                        <TextField id="outlined-basic"
+                                   fullWidth={true}
+                                   label="id"
+                                   variant="outlined"
+                                   value={id}
+                                   onChange={handlerOnChangeId}
+                        />
+                    </div>
+                    <div className={styles.input}>
+                        <TextField id="outlined-basic"
+                                   fullWidth={true}
+                                   label="email"
+                                   variant="outlined"
+                                   onChange={handlerOnChangeEmail}
+                        />
+                    </div>
+                    <div className={styles.input}>
+                        <TextField id="outlined-basic"
+                                   fullWidth={true}
+                                   label="Имя"
+                                   variant="outlined"
+                                   onChange={handlerOnChangeName}
+                        />
+                    </div>
+                    <div className={styles.input}>
+                        <TextField id="outlined-basic"
+                                   fullWidth={true}
+                                   label="Фамилия"
+                                   variant="outlined"
+                                   onChange={handlerOnChangeLastName}
+                        />
+                    </div>
+                    <div className={styles.input}>
+                        <TextField id="outlined-basic"
+                                   fullWidth={true}
+                                   label="Дата рождения"
+                                   variant="outlined"
+                                   onChange={handlerOnChangeBirthDate}
+                        />
+                    </div>
+                    <div className={styles.checkBox}>
+                        <Checkbox checked={access}
+                                  onChange={handlerOnChangeAccess}
+                                  aria-label={'Доступ'}
+                        />
+                        <Typography className={styles.titleCheckBox}
+                                    id="modal-modal-title"
+                                    variant="body1"
+                        >
+                            Доступ
+                        </Typography>
+                    </div>
+
                     <div className={styles.boxButtonModal}>
                         <ButtonAdd open={open} setOpen={setOpen}/>
                         <ButtonClose open={open} setOpen={setOpen}/>
